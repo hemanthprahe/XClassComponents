0283d78ad4a5dd2e2003a1a09761aaf560d43f12
hemanthprahe <141354569+hemanthprahe@users.noreply.github.com>
2024-07-30 10:48:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# XClassComponents
+# XClassComp
+link---->https://kaarthiikm-x-class-comp-counter.vercel.app/
---
+++ xclasscompcounter/src/App.js
@@ -0,0 +1,11 @@
+import CounterClass from "./CounterClass";
+
+function App() {
+  return (
+    <div>
+      <CounterClass/>
+    </div>
+  );
+}
+
+export default App;
